3bb5074bc76a8c0060380bc8d108bdb4bc5315b8
evohox <evohox@mail.ru>
2025-05-22 13:39:45 +0700

    Added new vesion of GPIO

--- RpyGPIO.py
+++ RpyGPIO.py
@@ -8,10 +8,7 @@ import os
 from Tournament import Tournament
 
 load_dotenv()
-login=os.getenv("login")
-password=os.getenv("password")
-api_url=os.getenv("api_url")
-fight_id = os.getenv("fight_id")
+api_url=os.getenv("API_URL")
 
 class GPIOHandler(QObject):
     # Сигналы для внешних событий
@@ -19,8 +16,12 @@ class GPIOHandler(QObject):
     fight_stopped = pyqtSignal()
 
     def __init__(self):
-        # self.tournament = Tournament(id=fight_id, login=login, password=password, api_url=api_url)
+        self.tournament = Tournament(
+            api_url=api_url
+        )
         self.lock = threading.Lock()
+        self.threads = []
+
         # Настройки по умолчанию
         self.LED_COUNT = 180
         self.LED_PIN = 18
@@ -36,17 +37,10 @@ class GPIOHandler(QObject):
         self.STATE_FIGHT = 2
         self.PREPARING = 3
 
-
         super().__init__()
         self.current_state = self.STATE_WAITING
         self.team1_ready = False
         self.team2_ready = False
-        self.team1_ready_event = threading.Event()
-        self.team2_ready_event = threading.Event()
-        self.team1_stop_event = threading.Event()
-        self.team2_stop_event = threading.Event()
-        self.referee_start_event = threading.Event()
-        self.referee_stop_event = threading.Event()
 
 
         # Настройка пинов для кнопок
@@ -76,30 +70,33 @@ class GPIOHandler(QObject):
         for button in self.buttons:
             GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-        self._running = False
-        self._task = None
+        self._running = True
 
     def run_loop(self):
         """Основной цикл обработки кнопок"""
         print("Starting!")
         try:
         # Инициализация - синий цвет
-            self.set_color(Color(0, 0, 255))
-            while True:
+            self.set_color(Color(0, 0, 0))
+            # self.set_color(Color(0, 0, 255))
+            threading.Thread(target=self.circle_color, args=(Color(0, 0, 255), Color(0, 255, 0))).start()
+            # self.circle_color(Color(0, 0, 255), Color(255, 0, 0))
+            while self._running:
                 # Проверка всех кнопок
                 for button in self.buttons:
                     if GPIO.input(button) == GPIO.HIGH:
-                        # self.handle_button_press(button)
-                        threading.Thread(target=self.handle_button_press, args=(button, )).start()
-                        time.sleep(0.1)  # Задержка для антидребезга
+                        t = threading.Thread(target=self.handle_button_press, args=(button, ))
+                        t.start()
+                        self.threads.append(t)
+
+                        time.sleep(0.1)
                         print(self.current_state, self.team1_ready, self.team2_ready, button)
 
 
                 time.sleep(0.05)
 
         except KeyboardInterrupt:
-            self.set_color(Color(0, 0, 0))  # Выключить все светодиоды
-            GPIO.cleanup()
+            self.stop()
             print("Программа завершена.")
 
     def handle_button_press(self, button):
@@ -110,38 +107,26 @@ class GPIOHandler(QObject):
             self.fade_to_color(Color(0, 255, 0), team=1)  # Зеленый
             if self.team2_ready:
                 self.current_state = self.STATE_READY
-            # result = self.tournament.set_ready("565", "first_ready", "https://grmvzdlx-3008.euw.devtunnels.ms")
-            # print(result)
-            # result = self.tournament.reload_page("https://grmvzdlx-3008.euw.devtunnels.ms")
-            # print(result)
+            self.tournament.send_team1_ready()
         elif button == self.TEAM2_READY and self.current_state == self.PREPARING and not self.team2_ready:
             self.team2_ready = True
             self.fade_to_color(Color(0, 255, 0), team=2)  # Зеленый
             if self.team1_ready:
                 self.current_state = self.STATE_READY
-            # result = self.tournament.set_ready("565", "second_ready", "https://grmvzdlx-3008.euw.devtunnels.ms")
-            # print(result)
-            # result = self.tournament.reload_page("https://grmvzdlx-3008.euw.devtunnels.ms")
-            # print(result)
+            self.tournament.send_team2_ready()
         elif button == self.REFEREE_START and self.current_state != self.STATE_FIGHT:
+            self.fight_started.emit()
             if self.current_state == self.STATE_WAITING:
                 self.current_state = self.PREPARING
-                self.fight_started.emit()
+                self.tournament.send_preparing()
             else:
                 self.current_state = self.STATE_FIGHT
-                self.fight_started.emit()
-                self.fade_to_color(Color(255, 0, 0), team=0, duration=2)  # Красный
-            # result = self.tournament.setfight("565", "2", "https://grmvzdlx-3008.euw.devtunnels.ms")
-            # print(result)
-            # result = self.tournament.reload_page("https://grmvzdlx-3008.euw.devtunnels.ms")
-            # print(result)
+                self.fade_to_color(Color(255, 0, 0), team=0)  # Красный
+                self.tournament.send_fight_start()
         elif (button in [self.TEAM1_STOP, self.TEAM2_STOP, self.REFEREE_STOP]) and self.current_state != self.STATE_WAITING:
             self.fight_stopped.emit()
             self.reset_to_waiting()
-            # result = self.tournament.setfight("565", "3", "https://grmvzdlx-3008.euw.devtunnels.ms")
-            # print(result)
-            # result = self.tournament.reload_page("https://grmvzdlx-3008.euw.devtunnels.ms")
-            # print(result)
+            self.tournament.send_fight_end()
 
 
     def space_handler(self):
@@ -154,21 +139,19 @@ class GPIOHandler(QObject):
             self.current_state = self.STATE_WAITING
             self.set_color(Color(0, 0, 255)) # Синий
 
-
     def set_color(self, color, team=0):
         """Установка цвета всей ленты"""
-        if team == 1:
-                for i in range(90, self.strip.numPixels()):
+        with self.lock:
+            if team == 1:
+                    for i in range(90, self.strip.numPixels()):
+                        self.strip.setPixelColor(i, color)
+            elif team == 2:
+                for i in range(self.strip.numPixels()-90):
                     self.strip.setPixelColor(i, color)
-        elif team == 2:
-            for i in range(self.strip.numPixels()-90):
-                self.strip.setPixelColor(i, color)
-        else:
-            for i in range(self.strip.numPixels()):
-                self.strip.setPixelColor(i, color)
-        self.strip.show()
-        # if duration > 0:
-        #     time.sleep(duration)
+            else:
+                for i in range(self.strip.numPixels()):
+                    self.strip.setPixelColor(i, color)
+            self.strip.show()
 
     def fade_to_color(self, target_color, team=0, duration=0.5):
         """Плавный переход к указанному цвету"""
@@ -187,22 +170,45 @@ class GPIOHandler(QObject):
         target_g = (target_color >> 8) & 0xff
         target_b = target_color & 0xff
 
-        with self.lock:
-            for step in range(steps):
-                r = int(current_r + (target_r - current_r) * (step / steps))
-                g = int(current_g + (target_g - current_g) * (step / steps))
-                b = int(current_b + (target_b - current_b) * (step / steps))
+        # with self.lock:
+        for step in range(steps):
+            r = int(current_r + (target_r - current_r) * (step / steps))
+            g = int(current_g + (target_g - current_g) * (step / steps))
+            b = int(current_b + (target_b - current_b) * (step / steps))
 
-                self.set_color(Color(r, g, b), team=team)
-                time.sleep(delay)
+            self.set_color(Color(r, g, b), team=team)
+            time.sleep(delay)
 
-    def blink(self, target_color, team=0, duration=2.0):
-        """Мигание цветом"""
+    def circle_color(self, first_color: Color, second_color: Color, frequency:int=100):
+        """Движение цветной линии по кругу"""
+        delay = 1 / frequency
+        line_id = 0
+        current_color = first_color
+        while self.current_state == self.STATE_WAITING and self._running:
+            for i in range(self.LED_COUNT):
+                pos = (i - line_id) % self.LED_COUNT
+                if pos <= self.LED_COUNT // 2:
+                    current_color = second_color
+                else:
+                    current_color = first_color
+                self.strip.setPixelColor(i, current_color)
 
-        current_color = self.strip.getPixelColor(0)
+            self.strip.show()
+            line_id += 1
+            time.sleep(delay)
 
-        self.fade_to_color(target_color, team=team, duration=duration/2)
-        self.fade_to_color(current_color, team=team, duration=duration/2)
+
+
+
+
+
+    # def blink(self, target_color, team=0, duration=2.0):
+    #     """Мигание цветом"""
+
+    #     current_color = self.strip.getPixelColor(0)
+
+    #     self.fade_to_color(target_color, team=team, duration=duration/2)
+    #     self.fade_to_color(current_color, team=team, duration=duration/2)
 
     def reset_to_waiting(self):
         """Сброс в состояние ожидания"""
@@ -213,13 +219,8 @@ class GPIOHandler(QObject):
 
     def stop(self):
         print("Stoping")
+        for t in self.threads:
+            t.join(timeout=0.1)
         self.set_color(Color(0, 0, 0))
         self._running = False
         GPIO.cleanup()
-
-def main():
-	gpio_handler = GPIOHandler()
-	gpio_handler.run_loop()
-
-if __name__ == "__main__":
-  main()
--- Tournament.py
+++ Tournament.py
@@ -1,65 +1,84 @@
 import requests
 import json
+import socketio
+import os
+from dotenv import load_dotenv
 
 class Tournament:
-    def __init__(self, id, login, password, api_url):
+    def __init__(self, api_url):
         self.api_url = api_url
-        self.id = id
-        self.auth(login, password)
-        
-    def auth(self, login, password):
-        response = requests.get(f"{self.api_url}/login?login={login}&password={password}")
-        print(response.json().get('token'))
-        self.token = response.json().get('token')
-        self.fight_id = response.json().get('fight_id')
-        
-        
-    def set_fight(self, id, stat, apiurl):
-        url = f"{apiurl}/api/fights/{id}"
-        headers = {
-            "Content-Type": "application/json"
-        }
-        data = {
-            "fight_id": id,
-            "fight_state": stat
-        }
-        
-        response = requests.post(
-            url,
-            headers=headers,
-            cookies={"token": self.token},
-            data=json.dumps(data),
-            allow_redirects=True,
-            timeout=None
-        )
-        
-        return response.status_code
-    
-    def set_ready(self, id, stat, apiurl):
-        url = f"{apiurl}/api/fights/{id}"
-        headers = {
-            "Content-Type": "application/json"
-        }
-        data = {
-            "fight_id": id,
-            stat: "1"
-        }
-        
-        response = requests.post(
-            url,
-            headers=headers,
-            cookies={"token": self.token},
-            data=json.dumps(data),
-            allow_redirects=True,
-            timeout=None
-        )
-        
-        return response.status_code
-    
-    def reload_page(self, apiurl):
-        url = f"{apiurl}/events"
-        response = requests.post(
-            url
-        )
-        
-        return response.status_code
+        self.sio = socketio.Client()
+        self.is_connected = False
+
+        # Регистрация обработчиков событий
+        self.register_handlers()
+
+        # Подключение к серверу и авторизация
+        self.connect()
+
+
+    def register_handlers(self):
+        """Регистрирует все обработчики событий."""
+        @self.sio.event
+        def connect():
+            print("✅ Подключено к серверу.")
+            self.is_connected = True
+
+        @self.sio.event
+        def disconnect():
+            print("❌ Отключено от сервера.")
+            self.is_connected = False
+
+        #ID поединка
+        @self.sio.on("BACK-END: Fight ID sent.")
+        def get_fight_id(id):
+            self.id = id
+            print(f"ID поединка: {id}")
+
+    def connect(self):
+        """Подключается к серверу."""
+        try:
+            self.sio.connect(self.api_url, wait_timeout = 10)
+            self.is_connected = True
+        except Exception as e:
+            print(f"⚠️ Не удалось подключиться: {e}")
+            self.is_connected = False
+
+    def send_team1_ready(self):
+        """Отправляет готовность команды 1."""
+        if self.is_connected:
+            self.sio.emit("BUTTONS: Team 1 ready.", self.id)
+
+    def send_team2_ready(self):
+        """Отправляет готовность команды 2."""
+        if self.is_connected:
+            self.sio.emit("BUTTONS: Team 2 ready.", self.id)
+
+    def send_fight_start(self):
+        """Отправляет запрос на начало боя."""
+        if self.is_connected:
+            self.sio.emit("BUTTONS: Fight start.", self.id)
+
+    def send_fight_end(self):
+        """Отправляет запрос на завершение боя."""
+        if self.is_connected:
+            self.sio.emit("BUTTONS: Fight end.", self.id)
+
+    def send_preparing(self):
+        """Отправляет запрос на подготовку к бою."""
+        if self.is_connected:
+            self.sio.emit("BUTTONS: Preparing start.", self.id)
+
+    def disconnect(self):
+        """Отключается от сервера."""
+        if self.is_connected:
+            self.sio.disconnect()
+
+
+# Пример использования
+if __name__ == "__main__":
+    load_dotenv()
+    api_url=os.getenv("API_URL")
+    tournament = Tournament(
+        api_url=api_url
+    )
