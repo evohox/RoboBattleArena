5189fb401aafc87f37395af945b4f81dedbcd4e2
evohox <evohox@mail.ru>
2025-06-02 14:21:20 +0700

    prereleas

--- Tournament.py
+++ Tournament.py
@@ -36,7 +36,7 @@ class Tournament:
             self.id = id
             print(f"ID поединка: {id}")
 
-        @self.sio.on("BACK-END: Overlay data sent")
+        @self.sio.on("BACK-END: Overlay data sent.")
         def handle_external_data(data):
             print("[proxy] Получены данные:", data)
             try:
@@ -45,10 +45,6 @@ class Tournament:
 
             except Exception as e:
                 print("[timer] Ошибка при парсинге:", e)
-
-        @self.sio.on("*")
-        def catch_all(event, data=None):
-            print(f"[Catch-all] Событие: {event}, данные: {data}")
 
     def process_overlay_data(raw_data: Dict[str, Any]) -> Dict[str, Any]:
         return [raw_data.get("team1", "Команда 1"), raw_data.get("team2", "Команда 2")]
