ae92ecb514bf8936de9104f57e725a0d7e8b127f
evohox <evohox@mail.ru>
2025-06-03 19:00:43 +0700

    release

--- QtWindow/logic.py
+++ QtWindow/logic.py
@@ -22,8 +22,9 @@ class Window(QMainWindow, Ui_MainWindow):
     prepare_end = pyqtSignal()
     update_all = pyqtSignal()
 
+    # Инициализация
     def __init__(self):
-        super().__init__()  # Инициализируем родительский класс
+        super().__init__()
         self.setupUi(self)  # Настраиваем интерфейс
         self.connetctionStatus = 0
         if self.connetctionStatus == 0:
@@ -37,8 +38,8 @@ class Window(QMainWindow, Ui_MainWindow):
 
         # Инициализация переменных
         self.initial_time = self.set_preparation_time(self.preparation_time)
-        self.time_left = self.initial_time  # Оставшееся время
-        self.state = "Idle"  # Начальное состояние таймера
+        self.time_left = self.initial_time
+        self.state = "Idle"
         self.status = "Подготовка"
 
         self.team_names = ["Загрузка...", "Загрузка..."]
@@ -50,8 +51,9 @@ class Window(QMainWindow, Ui_MainWindow):
         # Устанавливаем время подготовки
         self.set_preparation_time(self.preparation_time)
 
-        self.update_time_label()  # Обновляем метку времени
+        self.update_time_label()
 
+    # Загрузка названий команд с сервера
     def load_team_names(self):
         while self.state == "Idle":
             team_names = self.tournament.get_team_names()
@@ -61,52 +63,62 @@ class Window(QMainWindow, Ui_MainWindow):
                 if self.state != "Idle":
                     break
 
-            time.sleep(0.1)  # Не грузим CPU
+            time.sleep(0.1)
 
+    # Берёт данные о названиях команд с сервера
+    def get_team_names(self):
+        self.team_names = self.tournament.get_team_names()
+        self.apply_settings()
+        print(self.team_names)
+
+    # Установка новых названий команд
     def on_team_names_loaded(self):
         self.apply_settings()
         self.update_time_label()
         print(f"Команды загружены: {self.team_names}")
 
+    # Перевод минут в секунды
     def set_preparation_time(self, minutes):
         return minutes * 60
 
+    # Обработчик ввода с клавиатуры
     def keyPressEvent(self, event):
-        """Обрабатываем нажатия клавиш."""
-        if event.key() == Qt.Key_Space:
+        if event.key() == Qt.Key_Space:  # Остановить таймер
             self.toggle_timer()
             self.space_btn.emit()
-        elif event.key() in (Qt.Key_R, Qt.Key_K):
+        elif event.key() in (Qt.Key_R):  # Перезапуск программы
             self.restart_window()
-        elif event.key() == Qt.Key_Escape:
+        elif event.key() == Qt.Key_Escape:  # Завершение программы
             self.tournament.disconnect()
             self.connetctionStatus = 0
             self.esc_btn.emit()
             QApplication.quit()
-        elif event.key() == Qt.Key_Left:
+        elif event.key() == Qt.Key_Left:  # Перемотка таймер на 5 секунд вперёд
             if self.time_left + 5 >= self.initial_time:
                 self.time_left = self.initial_time
             else:
                 self.time_left += 5
             self.pause_timer()
             self.update_time_label()
-        elif event.key() == Qt.Key_S:
-            self.show_settings_dialog()
-        elif event.key() == Qt.Key_Right:
+        elif event.key() == Qt.Key_Right:  # Перемотка таймер на 5 секунд назад
             if self.time_left - 5 <= 0:
                 self.time_left = 0
             else:
                 self.time_left -= 5
             self.pause_timer()
             self.update_time_label()
-        elif event.key() == Qt.Key_U:
+        elif event.key() == Qt.Key_S:  # Открывает диалоговое окно настроек
+            self.show_settings_dialog()
+        elif event.key() == Qt.Key_U:  # приводит систему в начальное положение
             self.update_window()
 
+    # Обработчик сигнала с старта с кнопок судьи
     def refery_handle(self):
         if self.status == "Подготовка" and self.state == "Ongoing":
             self.time_left = 0
         self.start_timer()
 
+    # Обработчик сигнала с кнопки сдаться
     def surrender(self):
         if self.status == "Подготовка":
             self.status = "Бой"
@@ -123,35 +135,36 @@ class Window(QMainWindow, Ui_MainWindow):
         except Exception as e:
             print(f"Ошибка отправки: {e}")
 
+    # Переключения режима таймер
     def toggle_timer(self):
-        """Запускаем или ставим на паузу таймер в зависимости от состояния."""
         if self.state == "Ongoing":
-            self.pause_timer()  # Пауза
+            self.pause_timer()
         else:
-            self.start_timer()  # Запускs
+            self.start_timer()
 
+    # Запуск таймера
     def start_timer(self):
-        """Запускаем таймер."""
-        self.state = "Ongoing"  # Меняем состояние на "Идет"
-        self.timer.start()  # Запускаем таймер
-        self.update_time_label()  # Обновляем метку времени
+        self.state = "Ongoing"
+        self.timer.start()
+        self.update_time_label()
 
+    # Остановка таймера
     def pause_timer(self):
-        """Ставим таймер на паузу."""
-        self.state = "Pause"  # Меняем состояние на "Пауза"
-        self.timer.stop()  # Останавливаем таймер
-        self.update_time_label()  # Обновляем метку времени
+        self.state = "Pause"
+        self.timer.stop()
+        self.update_time_label()
 
+    # Перезапуск программы
     def restart_window(self):
-        """Перезапускаем программу."""
         print("Перезапуск программы...")
         self.tournament.disconnect()
         self.connetctionStatus = 0
+        time.sleep(3)
         python = sys.executable
         os.execv(python, [python] + sys.argv)
 
+    # Приводит систему к начальному состоянию
     def update_window(self):
-        """ "Обновляет окно выставяя начальное положение"""
         self.state = "Idle"
         self.status = "Подготовка"
         self.initial_time = self.set_preparation_time(self.preparation_time)
@@ -159,8 +172,8 @@ class Window(QMainWindow, Ui_MainWindow):
         threading.Thread(target=self.load_team_names, daemon=True).start()
         self.update_time_label()
 
+    # Логика таймера
     def update_timer(self):
-        """Обновляем оставшееся время каждую секунду."""
         if self.time_left <= 0:
             if self.status == "Подготовка":
                 self.initial_time = 3 * 60
@@ -175,32 +188,27 @@ class Window(QMainWindow, Ui_MainWindow):
                     print(f"Ошибка отправки: {e}")
                 self.prepare_end.emit()
             else:
-                self.timer.stop()  # Останавливаем таймер, если время вышло
-                self.state = "End"  # Меняем состояние на "End"
-                self.update_time_label()  # Обновляем метку таймера
-                print("HEHEHE")
-                QTimer.singleShot(1000, self.update_window)  # Обновляем окно
+                self.timer.stop()
+                self.state = "End"
+                self.update_time_label()
+                QTimer.singleShot(30000, self.update_window)
         else:
             self.time_left -= 1  # Уменьшаем оставшееся время
-            self.update_time_label()  # Обновляем метку времени
+            self.update_time_label()
 
+    # Обновление вывода таймера
     def update_time_label(self):
-        """Обновляем текст метки времени в зависимости от состояния."""
         if self.state == "Idle":
             self.time_label.setFont(QFont("Bebas Neue", 90))
-            # Центрирование текста по горизонтали и вертикали
             self.time_label.setText(f'<div style="color:white; ">{self.status}</div>')
-
         elif self.state == "Ongoing":
             if self.time_left > self.initial_time:
                 self.time_label.setFont(QFont("Bebas Neue", 125))
-                # Центрирование текста по горизонтали и вертикали
                 self.time_label.setText('<div style="color:white; ">Старт!</div>')
             else:
                 self.time_label.setFont(QFont("Bebas Neue", 145))
                 minutes = self.time_left // 60
                 seconds = self.time_left % 60
-                # Центрирование текста по горизонтали и вертикали
                 if self.time_left <= 10:
                     self.time_label.setText(
                         f'<div style="color:red;">{minutes:02d}:{seconds:02d}</div>'
@@ -209,30 +217,19 @@ class Window(QMainWindow, Ui_MainWindow):
                     self.time_label.setText(
                         f'<div style="color:white;">{minutes:02d}:{seconds:02d}</div>'
                     )
-
         elif self.state == "Pause":
             if self.time_left > self.initial_time:
                 self.time_label.setFont(QFont("Bebas Neue", 125))
-                # Центрирование текста по горизонтали и вертикали
                 self.time_label.setText('<div style="color:white; ">Старт!</div>')
             else:
                 self.time_label.setFont(QFont("Bebas Neue", 145))
                 minutes = self.time_left // 60
                 seconds = self.time_left % 60
-                # Центрирование текста по горизонтали и вертикали
                 self.time_label.setText(
                     f'<div style="color:blue; ">{minutes:02d}:{seconds:02d}</div>'
                 )
-
         elif self.state == "End":
             self.time_label.setFont(QFont("Bebas Neue", 145))
-            # Центрирование текста по горизонтали и вертикали
             self.time_label.setText('<div style="color:white; ">Стоп!</div>')
-
         else:
             raise Exception("Error with state")
-
-    def get_team_names(self):
-        self.team_names = self.tournament.get_team_names()
-        self.apply_settings()
-        print(self.team_names)
